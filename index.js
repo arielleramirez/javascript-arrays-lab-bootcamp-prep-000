ec059b96b304251e6ed0559050885d89556a5c78
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-20 23:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = "I don't do much."
 var kittens =  ["Milo", "Otis", "Garfield"];
 
 destructivelyAppendKitten = kittens.append("Ralph");
-   
+   return kittens
   
-  }
+  
   
   function destructivelyPrependKitten(name) {
    kittens.prepend = "Bob"; 
